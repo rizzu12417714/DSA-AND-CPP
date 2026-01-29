ecd9a2e306178102c44c39eabf8d0972355a5a19
rizzu12417714 <rizzukhansns0786@gmail.com>
2026-01-29 23:02:49 +0530

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Square of the number is: " << num * num << endl;
+
+    return 0;
+}
+
